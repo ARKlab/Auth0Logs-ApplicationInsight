88b49f4e2071abe45199777978ee52bd70aab39c
Jacopo Cina <jacopo.cina@ark-energy.eu>
2023-05-04 10:05:03 +0200

    feat(auth0tpapplicationinsight): Added Deploy.json ARM file, changed telemetryclient.
    ref: #13409

---
+++ Services/Auth0Log/Auth0Log/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var host = new HostBuilder()
+            .ConfigureFunctionsWorkerDefaults()
+            .ConfigureHostConfiguration((config) =>
+            {
+                config.AddEnvironmentVariables();
+            })
+            .Build();
+
+        host.Run();
+    }
+}
